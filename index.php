a621c652308ff6d5191a298c0fd96ab19e025537
LennyHo <lennyyk27@gmail.com>
2026-01-24 01:50:54 +0800

    setting up for nickname and topic selected in index.php

--- index.php
+++ index.php
@@ -1,17 +1,46 @@
 <?php
-echo "Hello, World!";
+// Start the session.
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (empty($_POST['nickname'])) {
+        echo "<script>alert('Nickname is required. Please enter your nickname.'); window.location.href='index.php';</script>";
+        exit();
+    }
+    // Set session variables based on POST data.
+    $_SESSION['nickname'] = $nickname = trim($_POST['nickname']);
+    $_SESSION['overall_score'] = 0;
+    $_SESSION['selected_topic'] = $_POST['topic'];
+    header('Location: quiz.php');
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<html>
 
+<head>
+    <title>Quiz Game - Login</title>
+</head>
+
+<body>
+    <h1>Welcome to the ISIT307 Quiz!</h1>
+
+    <form method="post" action="index.php">
+        <label>Enter Nickname:</label>
+        <input type="text" name="nickname" required> <br><br>
+
+        <label>Select Topic:</label>
+        <select name="topic">
+            <option value="Animals">Animals</option>
+            <option value="Environment">Environment</option>
+        </select> <br><br>
+
+        <input type="submit" value="Start Quiz">
+    </form>
 </body>
+
 </html>
 
 <?php
---
+++ quiz.php
@@ -0,0 +1,18 @@
+<?php
+?><?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz Page</title>
+</head>
+<body>
+    <h2>you are logged into quiz page!</h2>
+</body>
+</html>
+
+<?php
+?>
